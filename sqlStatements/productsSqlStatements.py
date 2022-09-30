8308a167250535b40965866c9d32787e90042247
Aurelio Gonzalez <agonzalez@sisu.mx>
2022-09-30 01:16:27 -0500

    Se agregaron statements sql para productos

---
+++ sqlStatements/productsSqlStatements.py
@@ -0,0 +1,3 @@
+createProduct = "INSERT INTO products (name, almount, cost_price, public_cost) VALUES (?, ?, ?, ?)"
+
+updateAlmount = "UPDATE products SET name = ?, almount = ?, cost_price = ?, public_cost = ? WHERE name = ?"
